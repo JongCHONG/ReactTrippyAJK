b52fa5710c565e33b27b59edb1141554d7b73dfe
karimou cisse <karimou.cisse1@gmail.com>
2021-11-23 17:22:32 +0100

    add cities name and images

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import styled from "styled-components"
+const CityContainer = styled.div`
+    display : flex;
+    flex-wrap : wrap;
+    width: 100%;
+`
+const ImageContainer = styled.div`
+    width : 300px;
+    margin-left : 20px;
+`
+const Image = styled.img`
+    width : 100%;
+    height: 400px;
+`
 
 export default function CityCard() {
     const [city, setCity] = useState()
@@ -7,21 +21,27 @@ export default function CityCard() {
     useEffect(() => {
         fetch("https://trippy-konexio.herokuapp.com/api/home")
         .then(response => response.json())
-        .then(data => setCity(data.city))
+        .then(data => setCity(data.cities))
     },[])
     console.log("city: ",city);
-    // if(city.length === 0) {
-    //     return null
-    // }
+    if(!city) {
+        return null
+    }
     return (
-        <div>
+        <CityContainer>
             <h1>City card</h1>
-            {/* {city.map((cities) => {
-                return <div key={cities.name}>
-                        <img src={cities.source} alt={cities.name} />
+            {city.map((cities) => {
+                return <ImageContainer 
+                            key={cities.name}
+                            className={`cities_element ${cities.name === "Paris"? "paris_element" : ""}`}>
+                            <Image src={`https://trippy-konexio.herokuapp.com/${cities.source}`}
+                                alt={cities.name} 
+                                className={`city`}
+                            >
+                            </Image>
                         <h2>{cities.name}</h2>
-                    </div>
-            })} */}
-        </div>
+                    </ImageContainer>
+            })}
+        </CityContainer>
     )
 }
